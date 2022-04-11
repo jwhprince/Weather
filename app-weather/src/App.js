90af584404e9869ace12c741ba3691ff14841992
jwhprince <piratkaignat@gmail.com>
2022-04-11 20:22:03 +0500

    all work

--- app-weather/src/App.js
+++ app-weather/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Clear from './img/clear.jpg';
+import Cloudy from './img/cloudy.jpg';
+import Rainy from './img/rainy.jpg';
+import Snow from './img/snow.jpg';
+import Overcast from './img/overcast.jpg';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
    const [place, setPlace] = useState("new york");
-   const [placeInfo, setPlaceInfo] = useState({})
+   const [placeInfo, setPlaceInfo] = useState({});
+
+   useEffect(() => {
+    handleFetch();
+   }, []);
 
    const handleFetch = () => {
 
@@ -20,31 +30,49 @@ function App() {
        condition: data.current.condition.text
      })
      );
+
+     setPlace("");
    };
 
     
 
+   
 
   return (
-    <div className="App">
+    <div
+     className="app"
+      style={
+        placeInfo.condition?.toLowerCase() === "clear" ||
+        placeInfo.condition?.toLowerCase() === "sunny"
+        ? {backgroundImage: `url(${Clear})`}
+        : placeInfo.condition?.includes('cloudy')
+        ? {backgroundImage: `url(${Cloudy})`}
+        : placeInfo.condition?.toLowerCase().includes("rainy")
+        ? {backgroundImage: `url(${Rainy})`}
+        : placeInfo.condition?.toLowerCase().includes("snow")
+        ? {backgroundImage: `url(${Snow})`}
+        : {backgroundImage: `url(${Overcast})`}
+      }
+    >
       <div className="search-input">
         <input
          type="text"
          value={place}
          onChange={(e) => setPlace(e.target.value)} 
          />
-        <button onClick={handleFetch}>Search</button>
+         <SearchIcon onClick={handleFetch} fontSize="large" className="search-button"/>
+        
       </div>
      <div className="weather-container">
         <div className="top-part">
-          <h1>{placeInfo.farenheit?.current}</h1>
+          <h1>{placeInfo.farenheit?.current}° F</h1>
           <div className="condition-high-low">
              <h1>{placeInfo.condition}</h1>
-             <h1>{placeInfo.farenheit?.hight}</h1>
-             <h1>{placeInfo.farenheit?.low}</h1>
+             <h1>{placeInfo.farenheit?.high}° F</h1>
+             <h1>{placeInfo.farenheit?.low}° F</h1>
           </div>
         </div>
-        <h2>{placeInfo.name}, {placeInfo.country}</h2>
+        <h2> {placeInfo.name}, {placeInfo.country} </h2>
      </div>
     </div>
   );
